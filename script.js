5fe56d44cd9b5b2f72c45f4912bd30820da5f459
wesbdss <wesleybenicio4@gmail.com>
2019-05-01 15:24:10 -0300

    Update: Altura individual da lama

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ var c = document.getElementById("screen");
 var ctx = c.getContext("2d");
 //matriz[x*matrix+y]
 var lamainfo = {	
-	'alturaLama': 36,
-	'alturaTerreno': 890
+	'tamanhoInicial': 36
 };
 
 var mapax = 25;//tamanho das linhas linhas
@@ -11,8 +10,8 @@ var mapay = 23;//tamanho das colunas
 var mapa = [1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,
             1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,
             1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,
-            1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000, 872,   1,   1,1000,
-            1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000, 872,   1,   1,1000,
+            1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000, 872, 872, 872,1000,
+            1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000, 872, 872, 872,1000,
             1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000, 840, 836,1000,1000,
             1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000, 830, 830,1000,1000,
             1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000, 825, 825,1000,1000,
@@ -32,14 +31,47 @@ var mapa = [1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,100
             1000,1000, 733, 733, 747, 747,1000,1000, 756, 756,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,
             1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000];
 
-function gerarMapa(matrix,matriy,values){ //tamanho da matriz x & tamanho da matriz y & valores
+var mapaLama = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,lamainfo.tamanhoInicial,lamainfo.tamanhoInicial,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,lamainfo.tamanhoInicial,lamainfo.tamanhoInicial,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+            	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];//representação da localização da lama e sua altura/força
+
+
+function gerarMapa(matrix,matriy,valuesAltura,valuesLama){ //tamanho da matriz x & tamanho da matriz y & valores
 	var tamx=20, tamy=20;//tamanho do retangulo
-	mat = [matrix*matriy];//matriz a ser executada
-	mat = values;//valores da matriz
+	mat = [matrix*matriy];
+	mat1 = [matrix*matriy];//matriz a ser executada
+	mat = valuesAltura;//valores da matriz de terreno
+	mat1 = valuesLama;
 	if(mat.length != matrix*matriy){
 		console.log("Valores Não Compatíveis com o tamanho da matrix");
 		console.log("mat: "+mat);
-		console.log("values: "+values);
+		console.log("values: "+valuesAltura);
+		return -1;
+	}
+	if(mat1.length != matrix*matriy){
+		console.log("Valores Não Compatíveis com o tamanho da matrix");
+		console.log("mat: "+mat1);
+		console.log("values: "+valuesLama);
 		return -1;
 	}
 
@@ -78,8 +110,8 @@ function gerarMapa(matrix,matriy,values){ //tamanho da matriz x & tamanho da mat
 			else if(mat[x*matrix+y] <= 1124) ctx.fillStyle ="#ffc3fe";
 			else if(mat[x*matrix+y] <= 1165) ctx.fillStyle ="#ffd9fe";
 			else if(mat[x*matrix+y] >= 1165) ctx.fillStyle ="#fefefe";
-			if(mat[x*matrix+y] == 0) ctx.fillStyle ="#CD661D";
-			if(mat[x*matrix+y] == 1) ctx.fillStyle ="#8B4513";
+			if(mat1[x*matrix+y] >= 1) ctx.fillStyle ="#8B4513";
+			if(mat1[x*matrix+y] < 0) ctx.fillStyle ="#9f6200";
 
 			//Falta os especiais, casas, a represa e a lama campos neutros
 			ctx.fillRect(i, j, tamx, tamy);
@@ -90,53 +122,110 @@ function gerarMapa(matrix,matriy,values){ //tamanho da matriz x & tamanho da mat
 	}
 }
 
-function movLama(matrix,matriy,matriz){
-	var novoMapa = matriz.slice();
+function movLama(matrix,matriy,matrizMapa,matrizLama){
+	var novoMapa = matrizLama.slice();
 	var semocorrencias = true;
-	var media = 1,qtd = 1;//calcular a media da força da prox onda de lama
+	var media = 0,qtd = 0;//calcular a media da força da prox onda de lama
 	//arrumar o fato de que cada lama não possui seu próprio valor , talvez fazer a média ajude
-	if(lamainfo.tamanhoTotal <= 0){
+	if(lamainfo.inicial <= 0){
 		return -1;
 	}else{
+		//media da lama atual
 		for (var i = 0; i < matrix; i++){
 			for (var j = 0; j < matriy; j++){
-				if(matriz[i*matrix+j] == 1){
+				if(matrizLama[i*matrix+j] > 0){
+					media = matrizLama[i*matrix+j]; qtd++;
+					if(matrizLama[(i+1)*matrix+j] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					if(matrizLama[(i+1)*matrix+(j-1)] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					if(matrizLama[(i+1)*matrix+(j+1)] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					if(matrizLama[(i-1)*matrix+j] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					if(matrizLama[(i-1)*matrix+(j+1)] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					if(matrizLama[(i-1)*matrix+(j-1)] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					if(matrizLama[i*matrix+(j+1)] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					if(matrizLama[i*matrix+(j-1)] > 0){
+						media = media + matrizLama[(i+1)*matrix+j];qtd++;
+					}
+					matrizLama[i*matrix+j] = media/qtd;
+					media = 0;
+					qtd = 0;
+				}
+			}
+		}
+		for (var i = 0; i < matrix; i++){
+			for (var j = 0; j < matriy; j++){
+				if(matrizLama[i*matrix+j] > 0){
 					semocorrencias = false;
-					if(matriz[(i+1)*matrix+(j)] < lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i+1)*matrix+(j)] != 0 && matriz[(i+1)*matrix+(j)] != 1){
-						media = media + (lamainfo.alturaTerreno - matriz[(i+1)*matrix+(j)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i+1)*matrix+(j)] = 1;
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i+1)*matrix+(j)]){
+						if (novoMapa[(i+1)*matrix+(j)] > 0){
+							media = novoMapa[(i+1)*matrix+(j)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i+1)*matrix+(j)];
+							novoMapa[(i+1)*matrix+(j)] = media/2;
+						} else if(novoMapa[(i+1)*matrix+(j)] == 0) novoMapa[(i+1)*matrix+(j)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i+1)*matrix+(j)];	
 					}
-					if(matriz[(i-1)*matrix+(j)]< lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i-1)*matrix+(j)] != 0&& matriz[(i-1)*matrix+(j)] != 1){
-						media = media + (lamainfo.alturaTerreno - matriz[(i-1)*matrix+(j)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i-1)*matrix+(j)] = 1;	
+
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i+1)*matrix+(j-1)]){
+						if (novoMapa[(i+1)*matrix+(j-1)] > 0){
+							media = novoMapa[(i+1)*matrix+(j-1)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i+1)*matrix+(j-1)];
+							novoMapa[(i+1)*matrix+(j-1)] = media/2;
+						} else if(novoMapa[(i+1)*matrix+(j-1)] == 0) novoMapa[(i+1)*matrix+(j-1)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i+1)*matrix+(j-1)];	
 					}
-					if(matriz[(i)*matrix+(j+1)]< lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i)*matrix+(j+1)] != 0&& matriz[(i)*matrix+(j+1)] != 1){
-						media = media +  (lamainfo.alturaTerreno - matriz[(i)*matrix+(j+1)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i)*matrix+(j+1)] = 1;
+
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i+1)*matrix+(j+1)]){
+						if (novoMapa[(i+1)*matrix+(j+1)] > 0){
+							media = novoMapa[(i+1)*matrix+(j+1)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i+1)*matrix+(j+1)];
+							novoMapa[(i+1)*matrix+(j+1)] = media/2;
+						} else if(novoMapa[(i+1)*matrix+(j+1)] == 0) novoMapa[(i+1)*matrix+(j+1)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i+1)*matrix+(j+1)];	
 					}
-					if(matriz[(i)*matrix+(j-1)]< lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i)*matrix+(j-1)] != 0&& matriz[(i)*matrix+(j-1)] != 1){
-						media = media +  (lamainfo.alturaTerreno - matriz[(i)*matrix+(j-1)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i)*matrix+(j-1)] = 1;
+
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i-1)*matrix+(j)]){
+						if (novoMapa[(i-1)*matrix+(j)] > 0){
+							media = novoMapa[(i-1)*matrix+(j)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i-1)*matrix+(j)];
+							novoMapa[(i-1)*matrix+(j)] = media/2;
+						} else if(novoMapa[(i-1)*matrix+(j)] == 0) novoMapa[(i-1)*matrix+(j)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i-1)*matrix+(j)];	
 					}
-					if(matriz[(i+1)*matrix+(j+1)]< lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i+1)*matrix+(j+1)] != 0&& matriz[(i+1)*matrix+(j+1)] != 1){
-						media = media + (lamainfo.alturaTerreno - matriz[(i+1)*matrix+(j+1)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i+1)*matrix+(j+1)] = 1;	
+
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i-1)*matrix+(j+1)]){
+						if (novoMapa[(i-1)*matrix+(j+1)] > 0){
+							media = novoMapa[(i-1)*matrix+(j+1)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i-1)*matrix+(j+1)];
+							novoMapa[(i-1)*matrix+(j+1)] = media/2;
+						} else if(novoMapa[(i-1)*matrix+(j+1)] == 0) novoMapa[(i-1)*matrix+(j+1)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i-1)*matrix+(j+1)];	
 					}
-					if(matriz[(i-1)*matrix+(j+1)]< lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i-1)*matrix+(j+1)] != 0&& matriz[(i-1)*matrix+(j+1)] != 1){
-						media = media +  (lamainfo.alturaTerreno - matriz[(i-1)*matrix+(j+1)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i-1)*matrix+(j+1)] = 1;
+
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i-1)*matrix+(j-1)]){
+						if (novoMapa[(i-1)*matrix+(j-1)] > 0){
+							media = novoMapa[(i-1)*matrix+(j-1)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i-1)*matrix+(j-1)];
+							novoMapa[(i-1)*matrix+(j-1)] = media/2;
+						} else if(novoMapa[(i-1)*matrix+(j-1)] == 0) novoMapa[(i-1)*matrix+(j-1)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i-1)*matrix+(j-1)];	
 					}
-					if(matriz[(i-1)*matrix+(j-1)]< lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i-1)*matrix+(j-1)] != 0&& matriz[(i-1)*matrix+(j-1)] != 1){
-						media = media +  (lamainfo.alturaTerreno - matriz[(i-1)*matrix+(j-1)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i-1)*matrix+(j-1)] = 1;	
+
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i)*matrix+(j-1)]){
+						if (novoMapa[(i)*matrix+(j-1)] > 0){
+							media = novoMapa[(i)*matrix+(j-1)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i)*matrix+(j-1)];
+							novoMapa[(i)*matrix+(j-1)] = media/2;
+						} else if(novoMapa[(i)*matrix+(j-1)] == 0) novoMapa[(i)*matrix+(j-1)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i)*matrix+(j-1)];	
 					}
-					if(matriz[(i+1)*matrix+(j-1)]< lamainfo.alturaLama+lamainfo.alturaTerreno && matriz[(i+1)*matrix+(j-1)] != 0&& matriz[(i+1)*matrix+(j-1)] != 1){
-						media = media +  (lamainfo.alturaTerreno - matriz[(i+1)*matrix+(j-1)]) + lamainfo.alturaLama;qtd++;
-						novoMapa[(i+1)*matrix+(j-1)] = 1;
+
+					if(matrizMapa[i*matrix+j] + matrizLama[i*matrix+j] > matrizMapa[(i)*matrix+(j+1)]){
+						if (novoMapa[(i)*matrix+(j+1)] > 0){
+							media = novoMapa[(i)*matrix+(j+1)] + (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i)*matrix+(j+1)];
+							novoMapa[(i)*matrix+(j+1)] = media/2;
+						} else if(novoMapa[(i)*matrix+(j+1)] == 0) novoMapa[(i)*matrix+(j+1)] = (matrizMapa[i*matrix+j] + matrizLama[i*matrix+j]) - matrizMapa[(i)*matrix+(j+1)];	
 					}
-					novoMapa[i*matrix+j] = 0;
-					if(media/qtd !=1) lamainfo.alturaLama = media/qtd;
-					media = qtd = 1;
+					
+					novoMapa[i*matrix+j] = -1;
 				}
 
 			}
@@ -148,13 +237,13 @@ function movLama(matrix,matriy,matriz){
 
 var nextmapa;
 function Main(){
-	if(gerarMapa(mapax,mapay,mapa) == -1){
+	if(gerarMapa(mapax,mapay,mapa,mapaLama) == -1){
 		return -1;
 	}
 	var informes = document.getElementById("informes");
-	informes.innerHTML = "<strong>Informes</strong></br>Altura do terreno da Lama: "+lamainfo.alturaTerreno+"</br>Intensidade da Lama: "+lamainfo.alturaLama+" ";
-	nextmapa = mapa;
+	informes.innerHTML = "<strong>Informes</strong>" + "</br>Intensidade da Lama: "+lamainfo.tamanhoInicial+"";
 	informes.innerHTML = informes.innerHTML+ "</br>";
+	nextmapa = mapaLama;
 	for(var i=0; i<mapay; i++)
 	{
 	    for(var j=0; j<mapax; j++)
@@ -169,12 +258,12 @@ function Main(){
 }
 
 function proximo(){
-	nextmapa = movLama(mapax,mapay,nextmapa);
-	if(gerarMapa(mapax,mapay,nextmapa) == -1){
+	nextmapa = movLama(mapax,mapay,mapa,nextmapa);
+	if(gerarMapa(mapax,mapay,mapa,nextmapa) == -1){
 		alert("Ocorreu um erro");
 		return -1;
 	}
-	informes.innerHTML = "<strong>Informes</strong>" + "</br>Altura do terreno da Lama: "+lamainfo.alturaTerreno+"</br>Intensidade da Lama: "+lamainfo.alturaLama+"";
+	informes.innerHTML = "<strong>Informes</strong>" + "</br>Intensidade da Lama: "+lamainfo.tamanhoInicial+"";
 	informes.innerHTML = informes.innerHTML+ "</br>";
 	for(var i=0; i<mapay; i++)
 	{
